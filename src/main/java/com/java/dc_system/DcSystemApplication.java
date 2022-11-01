f371e7d1f82ca5bf0c40c4dbe319ec4175764789
Forest1sland <1548893870@qq.com>
2022-11-01 20:15:39 +0800

    update doc

--- src/main/java/com/java/dc_system/DcSystemApplication.java
+++ src/main/java/com/java/dc_system/DcSystemApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DcSystemApplication {
 
+    //不写代码
     public static void main(String[] args) {
         SpringApplication.run(DcSystemApplication.class, args);
+        System.out.println("zjt");
     }
 
 }
