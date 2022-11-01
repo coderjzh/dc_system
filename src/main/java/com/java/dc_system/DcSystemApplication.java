f812ee0d712aec1edfd235a86beb840ed183bc97
Forest1sland <1548893870@qq.com>
2022-11-01 09:46:11 +0800

    Initial commit

---
+++ src/main/java/com/java/dc_system/DcSystemApplication.java
@@ -0,0 +1,13 @@
+package com.java.dc_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/dc_system/DcSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.dc_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
